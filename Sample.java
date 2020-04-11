903feb5568230f096d35d51f55d59987359ae118
Michael Tran <tran1619@gmail.com>
2020-04-10 22:45:29 -0400

    change list to Integer type

--- Sample.java
+++ Sample.java
@@ -1,10 +1,11 @@
 import java.util.*;
-import java.language.*;
 
 class Sample {
     public static void main(final String[] args) {
-        final List numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        numbers.stream().filter(e -> e % 2 > 0).limit(5).forEach(System.out::println);
+        numbers.stream()
+            .filter(e -> e % 2 == 0)
+            .forEach(System.out::println);
     }
 }
