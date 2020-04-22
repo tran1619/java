8cb5c9ff0975b9999fc3c33dfaf6b25ff86784a1
git <michael@PC-88.localdomain>
2020-04-22 17:09:28 -0400

    Fixed groupingBy using Function identity method.

--- Sample2.java
+++ Sample2.java
@@ -1,19 +1,21 @@
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import static java.util.stream.Collectors.*;
 
 class Sample2 {
+
     public static void main(final String[] args) {
         final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 23, 22, 24, 1, 2);
 
-        final Map<String, Integer> intCount = 
+        // count the number times a number exist in the list
+        final Map<Integer, Long> integerCount = 
         numbers.stream()
-               .collect(toMap(String::toString));
+               .collect(groupingBy(Function.identity(), Collectors.counting()));
 
-        System.out.println(intCount);
-
-
-System.out.println(intCount);
+        System.out.println(integerCount);
                 
-}
+    }
 }
